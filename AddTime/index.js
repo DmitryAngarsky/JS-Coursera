d4a309a8ab255a973d48b591787b206503c7535a
DmitryAngarsky <53812480+DmitryAngarsky@users.noreply.github.com>
2020-09-30 22:00:30 +0400

    FirstWeek-3
    AddTime

---
+++ AddTime/index.js
@@ -0,0 +1,27 @@
+var HOURS_PER_DAY = 24;
+var MINUTES_PER_HOUR = 60;
+
+/**
+ * @param {Number} hours
+ * @param {Number} minutes
+ * @param {Number} interval
+ * @returns {String}
+ */
+module.exports = function (hours, minutes, interval) {
+    
+    minutes += interval;
+    hours += Math.floor(minutes / MINUTES_PER_HOUR);
+
+    minutes %= MINUTES_PER_HOUR;
+    hours %= HOURS_PER_DAY;
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    return hours + ":" + minutes;
+};
